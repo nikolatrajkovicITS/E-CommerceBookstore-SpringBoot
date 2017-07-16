e5ce6a0b3d9f7a5281242748774ffd2915b41467
Nikola Trajkovic <nikola.trajkovic@bluecouchdigital.com>
2017-07-16 21:08:31 +0200

    Added logic for ShopingCard

--- src/main/java/com/bookstore/controller/ShoppingCartController.java
+++ src/main/java/com/bookstore/controller/ShoppingCartController.java
@@ -6,15 +6,11 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
-import com.bookstore.domain.Book;
 import com.bookstore.domain.CartItem;
 import com.bookstore.domain.ShoppingCart;
 import com.bookstore.domain.User;
-import com.bookstore.service.BookService;
 import com.bookstore.service.CartItemService;
 import com.bookstore.service.ShoppingCartService;
 import com.bookstore.service.UserService;
@@ -28,9 +24,6 @@ public class ShoppingCartController {
 	
 	@Autowired
 	private CartItemService cartItemService;
-	
-	@Autowired
-	private BookService bookService;
 	
 	@Autowired
 	private ShoppingCartService shoppingCartService;
@@ -50,42 +43,4 @@ public class ShoppingCartController {
 		return "shoppingCart";
 	}
 
-	@RequestMapping("/addItem")
-	public String addItem(
-			@ModelAttribute("book") Book book,
-			@ModelAttribute("qty") String qty,
-			Model model, Principal principal
-			) {
-		User user = userService.findByUsername(principal.getName());
-		book = bookService.findOne(book.getId());
-		
-		if (Integer.parseInt(qty) > book.getInStockNumber()) {
-			model.addAttribute("notEnoughStock", true);
-			return "forward:/bookDetail?id="+book.getId();
-		}
-		
-		CartItem cartItem = cartItemService.addBookToCartItem(book, user, Integer.parseInt(qty));
-		model.addAttribute("addBookSuccess", true);
-		
-		return "forward:/bookDetail?id="+book.getId();
-	}
-	
-	@RequestMapping("/updateCartItem")
-	public String updateShoppingCart(
-			@ModelAttribute("id") Long cartItemId,
-			@ModelAttribute("qty") int qty
-			) {
-		CartItem cartItem = cartItemService.findById(cartItemId);
-		cartItem.setQty(qty);
-		cartItemService.updateCartItem(cartItem);
-		
-		return "forward:/shoppingCart/cart";
-	}
-	
-	@RequestMapping("/removeItem")
-	public String removeItem(@RequestParam("id") Long id) {
-		cartItemService.removeCartItem(cartItemService.findById(id));
-		
-		return "forward:/shoppingCart/cart";
-	}
 }
